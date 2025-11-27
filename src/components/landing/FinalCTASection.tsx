13f8e694270e7dfbe4719a60b42d7c6e663dae2c
Labyrinth-io <mmaxryann4@gmail.com>
2025-11-27 14:07:34 +1000

    Updated FinalCTASection.tsx

--- src/components/landing/FinalCTASection.tsx
+++ src/components/landing/FinalCTASection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
-import { ArrowRight } from "lucide-react";
+import { ArrowDown } from "lucide-react";
 
 export default function FinalCTASection() {
   const scrollToContact = () => {
