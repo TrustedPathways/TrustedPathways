16689b614d5bc7ff7bc46019ec1673609fbd9ba7
Labyrinth-io <mmaxryann4@gmail.com>
2025-11-27 16:58:59 +1000

    Updated Footer.tsx

--- src/components/landing/Footer.tsx
+++ src/components/landing/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
           <div>
             <div className="flex items-center gap-3 mb-6">
               <img
-                src="/src/components/Logo/TP.webp"
+                src="TrustedPathways/Public/TP.webp"
                 alt="Trusted Pathways Logo"
                 className="h-12 w-auto"
               />
